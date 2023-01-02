97619ef5694a6fefd512b74c4f651e241fd8e3da
Satya Sowri Sampath Korturti <korturti.s@northeastern.edu>
2023-01-02 19:08:42 +0530

    adding styles to the nav bar icons

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,7 +3,7 @@ import "./index.scss";
 import LogoS from "../../assets/images/logo-s.png";
 import LogoSubtitle from "../../assets/images/logo_sub.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { faHome,faUser,faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const Sidebar =() => (
 <div className="nav-bar">
@@ -13,7 +13,16 @@ const Sidebar =() => (
     </Link>
 <nav>
     <NavLink exact = "true" activeclassname="active" to="/">
+        {/* activeclassname is used to style the link */}
     <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
+    </NavLink>
+    <NavLink exact = "true" activeclassname="active" className="about-link" to="/about">
+        {/* activeclassname is used to style the link */}
+    <FontAwesomeIcon icon={faUser} color="#4d4d4e"/>
+    </NavLink>
+    <NavLink exact = "true" activeclassname="active" className="contact-link" to="/contact">
+        {/* activeclassname is used to style the link */}
+    <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
     </NavLink>
 </nav>
 </div>
