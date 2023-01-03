a51803c31466d8de6d706236aed1d81d65aeb498
Satya Sowri Sampath Korturti <korturti.s@northeastern.edu>
2023-01-03 17:07:36 +0530

    oulet component, layout styles and js

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,8 +1,21 @@
 import Sidebar from "../Sidebar/index.js";
 import "./index.scss";
-
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
-    return <Sidebar/>;
+    return (
+        <div className="App">
+            <Sidebar/>
+            <div className="page">   
+                <span className="tags top-tags">&lt;body&gt;</span>
+                <Outlet/>
+                {/* outlet component renders the nested components or links in place of the outlet component */}
+                <span className="tags bottom-tags">&lt;/body&gt;
+                <br />
+                <span className="bottom-tag-html">&lt;/html&gt;</span>
+                </span>
+            </div>
+        </div>
+    )
 }
 export default Layout;
