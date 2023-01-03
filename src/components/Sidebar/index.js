af30fa46574b4ab40213e9b6b23aca80ea1f5252
Satya Sowri Sampath Korturti <korturti.s@northeastern.edu>
2023-01-03 14:38:38 +0530

    adding the links

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,6 +4,7 @@ import LogoS from "../../assets/images/logo-s.png";
 import LogoSubtitle from "../../assets/images/logo_sub.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome,faUser,faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Sidebar =() => (
 <div className="nav-bar">
@@ -25,6 +26,22 @@ const Sidebar =() => (
     <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
     </NavLink>
 </nav>
+<ul>
+    <li>
+        <a target="_blank" rel= 'noreferrer' href='https://www.linkedin.com/in/sowri-sampath'>
+            {/* _blank opens up the link in the new window */}
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e"/>
+        </a>
+        <a target="_blank" rel= 'noreferrer' href='https://github.com/Sowri19'>
+            {/* _blank opens up the link in the new window */}
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e"/>
+        </a>
+        <a target="_blank" rel= 'noreferrer' href='https://www.instagram.com/sowriiii/'>
+            {/* _blank opens up the link in the new window */}
+            <FontAwesomeIcon icon={faInstagram} color="#4d4d4e"/>
+        </a>
+    </li>
+</ul>
 </div>
 )
 
