860866adab614c9a97c03cd9f1accaca9103cb55
Satya Sowri Sampath Korturti <korturti.s@northeastern.edu>
2023-01-10 08:57:03 -0600

    letter animation

--- src/components/AnimatedLetters/index.js
+++ src/components/AnimatedLetters/index.js
@@ -1,6 +1,9 @@
 import './index.scss';
 
-const AnimatedLetters = ({letters, strArray, idx}) => {
+// first argument is class applied to the characters
+// array of the characters
+// index is the starting point and also set the delay of the characters and the animation
+const AnimatedLetters = ({letterClass, strArray, idx}) => {
     return(
         <span>
             { strArray.map((char, i) => ((
---
+++ src/components/Home/Logo/index.js
@@ -0,0 +1,14 @@
+import './index.scss';
+import LogoS from '../../../assets/images/logo-s.png';
+const Logo = () => {
+    <div className ='Lgoo-Container'>
+        <img className='solid-Logo' src={LogoS} alt="s" />
+        <svg
+        width="559pt"
+        height={}
+        ></svg>
+    </div>
+}
+
+
+export default Logo
