d8eb6b9c4e4a738ce61bdcde1883194ac5b98eab
Satya Sowri Sampath Korturti <korturti.s@northeastern.edu>
2023-01-20 21:31:11 -0500

    animated about logo

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,9 @@
 import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
 import { useEffect, useState } from 'react';
+// import { faAws } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAws, faHtml5, faCss3, faReact, faJsSquare, faGitAlt } from '@fortawesome/free-brands-svg-icons';
 const About =()=>{
     const [letterClass,setLetterClass] = useState('text-animate');
     useEffect(()=>{
@@ -21,9 +24,28 @@ const About =()=>{
             <p>As a Master's student in Information Systems with a strong interest in front-end development, I possess the technical skills and knowledge to design and implement visually appealing and user-friendly web applications.</p>
             </div>
 
-            <div className="stage-cube-cont">
-                
+    <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faAws} color="#000000" />
             </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
+    </div>
         </div>
     )
 }
