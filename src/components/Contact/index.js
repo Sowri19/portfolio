2272a3b0f35ca81eab5783438ab7424a4f63cc2d
Satya Sowri Sampath Korturti <korturti.s@northeastern.edu>
2023-01-21 18:20:06 -0500

    form and styles

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -25,6 +25,41 @@ return (
           I'm looking for freelancing work, especially for challenging or big projects.
           However, please feel free to contact me using the form below if you have any other requests or queries.
           </p>
+          <div className="contact-form">
+            <form >
+              <ul>
+                <li className="half">
+                  <input placeholder="Name" type="text" name="name" required />
+                </li>
+                <li className="half">
+                  <input
+                    placeholder="Email"
+                    type="email"
+                    name="email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
         </div>
     </div>
     <Loader type= "pacman" />
