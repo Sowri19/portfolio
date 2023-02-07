2a0574ac18e77b42e0213360393616aee0c7ea3e
Satya Sowri Sampath Korturti <korturti.s@northeastern.edu>
2023-02-06 23:10:00 -0500

    Hero Section changes

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import css from "./Hero.module.scss";
+// import { motion } from "framer-motion";
+
+const Hero = () => {
+  return (
+    // adding the global styling class to the section "paddings"
+    <section className={`paddings ${css.wrapper}`}>
+      <div className={`innerWidth ${css.container}`}>
+        {/* Upper Elements */}
+        <div className={css.upperElements}>
+          <span className="primaryText">
+            Hey There,
+            <br />
+            I'm Sowri.
+          </span>
+
+          <span className="secondaryText">
+            I design beautiful simple
+            <br />
+            things, And I love what i do{" "}
+          </span>
+        </div>
+
+        <div className={css.person}>
+          <img src="./person1.png" alt="" />
+        </div>
+
+        <a className={css.email} href="mailto:sowri1219@gmail.com">
+          sowri1219@gmail.com
+        </a>
+
+        {/* Lower Elements */}
+        <div className={css.lowerElements}>
+          <div className={css.experience}>
+            <div className="primaryText">3</div>
+            <div className="secondaryText">
+              <div>Years</div>
+              <div>Experience</div>
+            </div>
+          </div>
+
+          <div className={css.certificate}>
+            <img src="./certificate.png" alt="" />
+            <span>CERTIFIED PROFESSIONAL</span>
+            <span>UI/UX DESIGNER</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
