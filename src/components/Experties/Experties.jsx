5474c072b16a9b7c1f11b74c3fb87df05b661a59
Satya Sowri Sampath Korturti <korturti.s@northeastern.edu>
2023-02-11 19:27:10 -0500

    experties

---
+++ src/components/Experties/Experties.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import css from './Experties.module.css';
+
+const Experties = () => {
+  return (
+    <section className={css.wrapper}>
+        div.{css.experties}
+    </section>
+  )
+}
+
+export default Experties
