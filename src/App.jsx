14b3915e38be600fdaeaa2d41542f3e4e83ef19c
Satya Sowri Sampath Korturti <98078922+Sowri19@users.noreply.github.com>
2023-06-09 09:19:24 +0530

    people section

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from "./components/hero/hero";
 import Experties from "./components/Experties/Experties";
 import Works from "./components/Works/Works";
 import Portfolio from "./components/Portfolio/Portfolio";
-
+import People from "./components/People/People";
 const App = () => {
   //don't forget to add font link in index.html
   return <div className= {`bg-primary ${css.container}`}>
@@ -13,6 +13,7 @@ const App = () => {
     <Experties/>
     <Works/>
     <Portfolio/>
+    <People/>
   </div>;
 };
 
